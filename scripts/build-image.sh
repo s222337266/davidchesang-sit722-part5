9c7c39ba808acd1bc2e954cd84cddacdf8acffb2
s222337266 <s222337266@deakin.edu.au>
2024-10-06 23:59:03 +0300

    Update docker file

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -3,6 +3,6 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile-prod .
+docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile .
 
-docker build -t $CONTAINER_REGISTRY/inventory-management:$VERSION --file ./Dockerfile-prod .
+docker build -t $CONTAINER_REGISTRY/inventory-management:$VERSION --file ./Dockerfile .
