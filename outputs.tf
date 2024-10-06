170a5c10b958dab86bac0eda3bd3eb4b14f06682
s222337266 <s222337266@deakin.edu.au>
2024-10-06 12:09:16 +0300

    Mark password output as sensitive

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,11 @@ output "acr_login_server" {
   value = azurerm_container_registry.container_registry.login_server
 }
 
-output "container_registry_admin_username" {
+output "acr_admin_username" {
   value = azurerm_container_registry.container_registry.admin_username
 }
 
-output "container_registry_admin_password" {
+output "acr_admin_password" {
   value = azurerm_container_registry.container_registry.admin_password
+  sensitive = true
 }
